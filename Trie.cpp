ce2cef8f7972075393d1b27d1f6f8fe686fdeb38
babubalaji877841@gmail.com <babubalaji877841@gmail.com>
2023-09-23 21:07:11 +0530

    commit

--- Trie.cpp
+++ Trie.cpp
@@ -20,7 +20,26 @@ class TrieNode
     }
 };
 
+class Trie
+{
+    public:
+    TrieNode* root;
+    void insertUtil(TrieNode* root,string word)
+    {
+        //base case
+        if(word.length()==0)
+        {
+            root->isTerminal=true;
+        }   
+        int index=word[0]-'A';
+    }
+
+    void insertWord(string word)
+    {
+        insertUtil(root,word);
+    }
+};
 int main()
 {
-    
+
 }
